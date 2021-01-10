e307f213a5165026bf29b8f90ef13835f61508a5
LY <1334850101@qq.com>
2021-01-10 17:19:14 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 //请联系@BotFather机器人注册自己的机器人，并填入下面的信息
-//重要，请勿泄露$token
-//$hookurl需要修改为自己的项目地址（网站必须使用https），修改完成后请访问"webhook.php"完成挂钩
+//请勿泄露$token，如果已泄露，请联系@BotFather机器人更改$token
+//$hookurl需要修改为自己的项目地址（网站必须使用https），完成后请访问webhook.php完成网络挂钩
+//数据库信息是可选的，用于部分功能，填入后请导入dic.sql数据库
+//$debug可能的值为0或1，用于开启日志记录和调试，默认关闭，填1开启
 $token = '';
 $connectroot = "https://api.telegram.org/bot{$token}/";
 $hookurl = 'https://这里修改为您的域名/telegram-bot/index.php';
