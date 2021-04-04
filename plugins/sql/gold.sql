43f313af702242783c606986246fd2c5e3b5c636
LY <1334850101@qq.com>
2021-04-04 09:38:41 +0800

    Add files via upload

---
+++ plugins/sql/gold.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- 主机： localhost
+-- 生成日期： 2021-04-04 09:25:01
+-- 服务器版本： 5.7.31-log
+-- PHP 版本： 7.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 数据库： `dic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `gold`
+--
+
+CREATE TABLE `gold` (
+  `id` bigint(20) NOT NULL COMMENT 'ID',
+  `number` text NOT NULL COMMENT '数量'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `gold`
+--
+ALTER TABLE `gold`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
